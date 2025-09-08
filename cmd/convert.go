d60763ded6f7007c453885a5c31ca6c6a85f5474
90135 <193311049+90135@users.noreply.github.com>
2025-09-08 14:34:24 +0800

    Make convert command support .tex and .nei

--- cmd/convert.go
+++ cmd/convert.go
@@ -12,17 +12,21 @@ var convertCmd = &cobra.Command{
 	Short: "Auto-detect and convert files",
 	Long: `Auto-detect and convert files between MOD and JSON formats.
 This command automatically determines the direction of conversion:
-- If the file is a MOD file, it will be converted to JSON
-- If the file is a JSON file that corresponds to a MOD file, it will be converted back to MOD
 
-Not supported: .tex.json and .tex
-  please use convert2tex and convert2image instead
+- If the file is a MOD file, it will be converted to JSON
+- If the file is a JSON file that corresponds to a MOD file, it will be converted to MOD
+- If the file is a tex file, it will be converted to image
+- If the file is an image file, it will be converted to tex
+- If the file is a nei file, it will be converted to csv
+- If the file is a csv file, it will be converted to nei
 
 This command can process a single file or all files in a directory.
 
 Examples:
   MeidoSerialization convert example.menu
   MeidoSerialization convert example.menu.json
+  MeidoSerialization convert example.tex
+  MeidoSerialization convert example.nei
   MeidoSerialization convert ./mixed_directory`,
 	Args: cobra.ExactArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
